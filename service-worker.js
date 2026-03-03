35561e14ae175a0c07fbe7e353d387ec4a70b07c
auditflow-pro <hadders83@icloud.com>
2026-03-03 07:34:32 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,18 @@
-const CACHE_NAME = "afp-cache-v3-4";
+const CACHE_NAME = "auditflow-v3.5";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./styles.css",
-        "./app.js"
-      ]);
-    })
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
-  return self.clients.claim();
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
