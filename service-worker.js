0d4f21328fb7d7e5ea27f030b27541a5e6e18d84
auditflow-pro <hadders83@icloud.com>
2026-03-04 07:15:39 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,49 @@
-const CACHE_NAME = "auditflow-v3.2";
+const CACHE="auditflow-v4"
 
-const CORE = [
-  "./",
-  "./index.html?v=3.2",
-  "./styles.css?v=3.2",
-  "./app.js?v=3.2",
-  "./manifest.json"
-];
+const ASSETS=[
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(CORE)));
-  self.skipWaiting();
-});
+"./",
+"./index.html?v=4.0",
+"./styles.css?v=4.0",
+"./app.js?v=4.0",
+"./manifest.json"
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
+]
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(c=>c.addAll(ASSETS))
+
+)
+
+self.skipWaiting()
+
+})
+
+self.addEventListener("activate",e=>{
+
+e.waitUntil(
+
+caches.keys().then(keys=>Promise.all(
+
+keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+
+))
+
+)
+
+self.clients.claim()
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(r=>r||fetch(e.request))
+
+)
+
+})
