f517fa60f41cf076dcf6a01cdbd1d92e8e48872c
auditflow-pro <hadders83@icloud.com>
2026-03-04 18:40:38 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,33 @@
-const CACHE_NAME="auditflow-v10.0"
+const CACHE_NAME="auditflow-v10.0";
 
 const urls=[
+
 "./",
-"./index.html?v=10.0",
-"./assessment.html?v=10.0",
+"./index.html",
+"./assessment.html",
+"./determination.html",
+"./report.html",
 "./styles.css?v=10.0",
-"./app.js?v=10.0",
-"./manifest.json?v=10.0"
-]
+"./app.js?v=10.0"
+
+];
 
 self.addEventListener("install",e=>{
-self.skipWaiting()
-e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urls)))
-})
 
-self.addEventListener("activate",e=>{
 e.waitUntil(
-caches.keys().then(keys=>
-Promise.all(
-keys.map(k=>{
-if(k!==CACHE_NAME)return caches.delete(k)
-})
-)
-)
-)
-self.clients.claim()
-})
+
+caches.open(CACHE_NAME).then(cache=>cache.addAll(urls))
+
+);
+
+});
 
 self.addEventListener("fetch",e=>{
+
 e.respondWith(
+
 caches.match(e.request).then(r=>r||fetch(e.request))
-)
-})
+
+);
+
+});
