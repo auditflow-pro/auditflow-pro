35944318de9725670c7b01ce9337837b813a58b6
auditflow-pro <hadders83@icloud.com>
2026-03-05 16:38:35 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,59 @@
 const CACHE_NAME = "auditflow-v7";
 
-const urlsToCache = [
-"/",
-"/index.html",
-"/assessment.html",
-"/determination.html",
-"/report.html",
-"/styles.css",
-"/app.js",
-"/manifest.json"
+const ASSETS = [
+
+"./",
+"./index.html",
+"./styles.css?v=7.0",
+"./app.js?v=7.0",
+"./manifest.json"
+
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install",event=>{
+
 event.waitUntil(
-caches.open(CACHE_NAME).then(cache => {
-return cache.addAll(urlsToCache);
+
+caches.open(CACHE_NAME).then(cache=>{
+
+return cache.addAll(ASSETS);
+
 })
+
 );
+
 });
 
-self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
+self.addEventListener("activate",event=>{
+
+event.waitUntil(
+
+caches.keys().then(keys=>{
+
+return Promise.all(
+
+keys.filter(key=>key!==CACHE_NAME)
+
+.map(key=>caches.delete(key))
+
 );
+
+})
+
+);
+
+});
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request).then(res=>{
+
+return res || fetch(event.request);
+
+})
+
+);
+
 });
